6987272c4e3b131cfc8ec3efe1d00f80f9dcda1d
Jacob <henriksen.jacob@gmail.com>
2025-07-30 00:31:30 +0200

    updateded html for new pages

--- backend/sql/game_users/create.sql
+++ backend/sql/game_users/create.sql
@@ -1,6 +1,6 @@
 create table if not exists game_users (
-  user_id bigint references users(id) on delete cascade,
-  game_id bigint references game(game_id) on delete cascade,
+  user_id integer references users(id) on delete cascade,
+  game_id uuid references game(game_id) on delete cascade,
   role text not null,
   primary key (user_id, game_id)
 );
